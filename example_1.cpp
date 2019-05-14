ff4ae7efa6e5cd4954bc0a47125df9e4f8965ca7
Todor Balabanov <todor.balabanov@gmail.com>
2019-05-14 20:13:06 +0300

    Unsuccessful attempt for improvement.

--- example_1.cpp
+++ example_1.cpp
@@ -11,13 +11,13 @@
 
 int main()
 {
-    // Creat Rastring's function in 5 dimensions
-    de::Rastrigin cost(5);
+    // Creat Rastring's function in 50 dimensions
+    de::Rastrigin cost(50);
 
     // Create Differential Evolution optimizer with population size of 50
     de::DifferentialEvolution de(cost, 50);
 
-    // Optimize for 200 iterations
+    // Optimize for 1000 iterations
     de.Optimize(1000, true);
 
     return 0;
